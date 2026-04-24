c856d56a9ebf547c9393e474a3f1039d1033c1dc
Rahul Ghosh <rahul.info305@gmail.com>
2026-04-24 18:47:50 +0600

    main content ui setup

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ st.divider()
 # Sidebar for image upload
 with st.sidebar:
     st.header("Upload Images")
-    uploaded_images = st.file_uploader("Choose images", accept_multiple_files=True, type=["jpg", "jpeg", "png"], key="image_upload")
+    uploaded_images = st.file_uploader(
+        "Choose images",
+        accept_multiple_files=True,
+        type=["jpg", "jpeg", "png"],
+        key="image_upload"
+    )
 
     # Display uploaded images
     if uploaded_images:
@@ -17,17 +22,40 @@ with st.sidebar:
             st.error("Please upload only 3 images.")
         else:
             col = st.columns(len(uploaded_images))
-        for image in uploaded_images:
-            with col[uploaded_images.index(image)]:
-                st.image(image, caption=image.name, output_format="auto")
+            for i, image in enumerate(uploaded_images):
+                with col[i]:
+                    st.image(image, caption=image.name, output_format="auto")
 
-    # Dificulty level selection
+    # Difficulty level selection
     st.header("Quiz Difficulty Level")
-    selected_difficulty = st.selectbox("Select difficulty level", options=["Easy", "Medium", "Hard"], key="difficulty", index=None)
+    selected_difficulty = st.selectbox(
+        "Select difficulty level",
+        options=["Easy", "Medium", "Hard"],
+        key="difficulty",
+        index=None
+    )
 
-    # Button to create summary and quiz
+    # Button
     pressed = st.button("Create Summary & Quiz Generator", key="create_button", type="primary")
 
+# Main Content
+if pressed:
+    if not uploaded_images:
+        st.error("You must upload at least 1 image.")
+    elif not selected_difficulty:
+        st.error("You must select a difficulty level.")
+    else:
+        # Note
+        with st.container(border=True):
+            st.subheader("Image Summary", anchor=False)
+            st.text("This note comes from gemini api")
 
+        # Audio Transcript
+        with st.container(border=True):
+            st.subheader("Audio Transcription", anchor=False)
+            st.text("This note comes from gemini api")
 
-
+        # Quiz Generate
+        with st.container(border=True):
+            st.subheader(f"Your {selected_difficulty} Quiz", anchor=False)
+            st.text("This note comes from gemini api")
